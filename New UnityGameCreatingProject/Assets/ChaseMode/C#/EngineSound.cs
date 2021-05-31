d862b56a459bfeec33c73d8633aba95102c6ee06
okayu-QB <84308950+okayu-QB@users.noreply.github.com>
2021-05-31 21:31:31 +0900

    AddSE

---
+++ New UnityGameCreatingProject/Assets/ChaseMode/C#/EngineSound.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineSound : MonoBehaviour
+{
+    public AudioClip Engine;
+    public AudioClip Hit;
+    public AudioClip Boost;
+    public Vihecle VihecleObj;
+    public VihecleHit VihecleHitObj;
+    AudioSource audioSource;
+
+    bool HitSECheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetAndPlaySE(AudioClip clip,bool loop)
+    {
+        if(audioSource == null)
+        {
+            Debug.Log("AudioSouce Component is null");
+            return;
+        }
+
+        if (audioSource.clip == clip)
+        {
+            return;
+        }
+
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
+        audioSource.clip = clip;
+        audioSource.loop = loop;
+        audioSource.Play();
+
+        Debug.Log("Play " + clip.ToString());
+    }
+
+    private void FixedUpdate()
+    {
+        if (VihecleHitObj.FrontHit == true)
+        {
+            HitSECheck = true;
+            SetAndPlaySE(Hit, false);
+        }
+        else if (VihecleObj.BoostMode == true)
+        {
+            if (audioSource.isPlaying && HitSECheck)
+            {
+                return;
+            }
+            else
+            {
+                SetAndPlaySE(Boost, true);
+                HitSECheck = false;
+            }
+        }
+        else if (VihecleObj.throttle != 0)
+        {
+            if (audioSource.isPlaying && HitSECheck)
+            {
+                return;
+            }
+            else
+            {
+                SetAndPlaySE(Engine, true);
+                HitSECheck = false;
+            }
+        }
+    }
+}
