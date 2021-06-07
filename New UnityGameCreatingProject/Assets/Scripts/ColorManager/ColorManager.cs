a4e98bdd1ec144bf82755e923f79cecfd8b03ad6
umaiga <53830457+umaiga@users.noreply.github.com>
2021-06-07 19:15:40 +0900

    conflictTest

---
+++ New UnityGameCreatingProject/Assets/Scripts/ColorManager/ColorManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+
+    public Collider playerCollider;
+    //public Collider collorR;
+    //public Collider collorG;
+    //public Collider collorB;
+
+    int readCount = 0;
+    int greenCount = 0;
+    int blueCount=0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
