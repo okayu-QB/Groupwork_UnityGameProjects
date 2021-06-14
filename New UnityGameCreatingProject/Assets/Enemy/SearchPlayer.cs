54836e721f01111ecfa55c10f0922005af3d4cff
okayu-QB <84308950+okayu-QB@users.noreply.github.com>
2021-06-14 16:11:44 +0900

    log

---
+++ New UnityGameCreatingProject/Assets/Enemy/SearchPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchPlayer : MonoBehaviour
+{
+    private Enemy Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponentInParent<Enemy>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //プレイヤーを発見
+        if(other.tag == "Player")
+        {
+            //パトカーの状態を取得
+            Enemy.EnemyState state = Enemy.GetState();
+            //パトカーがチェイス状態でなければ、チェイス状態に変更
+            if(state != Enemy.EnemyState.Chase)
+            {
+                Debug.Log("Find Enemy");
+                moveEnemy.Set
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
