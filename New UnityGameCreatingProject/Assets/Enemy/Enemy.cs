7a7da5709fd9493d7223e67bd51e0751697e776f
okayu-QB <84308950+okayu-QB@users.noreply.github.com>
2021-06-20 03:14:20 +0900

    log

--- New UnityGameCreatingProject/Assets/Enemy/Enemy.cs
+++ New UnityGameCreatingProject/Assets/Enemy/Enemy.cs
@@ -7,11 +7,24 @@ public class Enemy : MonoBehaviour
     public enum EnemyState
     {
         Patrol,
+        Wait,
         Chase
-    }
+    };
 
+    //目的地
+    private Vector3 destination;
+    //パトカーの速さ
+    private Vector3 velocity;
+    //移動方向
+    private Vector3 direction;
+    //到着フラグ
+    private bool arrived;
+    //待ち時間
+    public float waitTime = 5f;
+    //プレイヤーのTransform
+    private Transform playerTransform;
+    //パトカーの状態
     private EnemyState state;
-
 
     //パトカーの状態変更メソッド
     public void SetState(EnemyState tempState, Transform targetObj = null)
@@ -32,5 +45,17 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        //巡回or追跡状態の判定
+        if(state == EnemyState.Patrol || state == EnemyState.Chase)
+        {
+            if(state == EnemyState.Chase)
+            {
+
+            }
+        }
     }
 }
