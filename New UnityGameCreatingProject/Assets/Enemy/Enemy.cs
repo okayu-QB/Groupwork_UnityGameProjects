892c2f5732cd28f1811a68f5a09e1d02fd43ec22
okayu-QB <84308950+okayu-QB@users.noreply.github.com>
2021-06-21 15:05:23 +0900

    logUpdeda

--- New UnityGameCreatingProject/Assets/Enemy/Enemy.cs
+++ New UnityGameCreatingProject/Assets/Enemy/Enemy.cs
@@ -11,6 +11,8 @@ public class Enemy : MonoBehaviour
         Chase
     };
 
+    //パトカーの移動速度
+    public float speed;
     //目的地
     private Vector3 destination;
     //パトカーの速さ
@@ -19,8 +21,12 @@ public class Enemy : MonoBehaviour
     private Vector3 direction;
     //到着フラグ
     private bool arrived;
+    //SetPositionスクリプト
+    private SetPosition setPosition;
     //待ち時間
     public float waitTime = 5f;
+    //　経過時間
+    private float elapsedTime;
     //プレイヤーのTransform
     private Transform playerTransform;
     //パトカーの状態
@@ -54,8 +60,32 @@ public class Enemy : MonoBehaviour
         {
             if(state == EnemyState.Chase)
             {
+                setPosition.SetDestination(playerTransform.position);
+                direction = (setPosition.GetDestination() - transform.position).normalized;
+                transform.LookAt(new Vector3(setPosition.GetDestination().x, transform.position.y, setPosition.GetDestination().z));
+                velocity = direction * speed;
+            }
 
+            //　目的地に到着したかどうかの判定
+            if (Vector3.Distance(transform.position, setPosition.GetDestination()) < 0.5f)
+            {
+                SetState(EnemyState.Wait);
+                //animator.SetFloat("Speed", 0.0f);
+            }
+            //　到着していたら一定時間待つ
+        }
+        else if (state == EnemyState.Wait)
+        {
+            elapsedTime += Time.deltaTime;
+
+            //　待ち時間を越えたら次の目的地を設定
+            if (elapsedTime > waitTime)
+            {
+                SetState(EnemyState.Walk);
             }
         }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        enemyController.Move(velocity * Time.deltaTime);
+    }
     }
 }
--- New UnityGameCreatingProject/Assets/Enemy/SearchPlayer.cs
+++ New UnityGameCreatingProject/Assets/Enemy/SearchPlayer.cs
@@ -18,13 +18,13 @@ public class SearchPlayer : MonoBehaviour
         if(other.tag == "Player")
         {
             //パトカーの状態を取得
-            //Enemy.EnemyState state = Enemy.GetState();
+            Enemy.EnemyState state = Enemy.GetState();
             //パトカーがチェイス状態でなければ、チェイス状態に変更
-            //if(state != Enemy.EnemyState.Chase)
-            //{
-            //    Debug.Log("Find Enemy");
-            //    Enemy.SetState(Enemy.EnemyState.Chase, other.transform);
-            //}
+            if(state != Enemy.EnemyState.Chase)
+            {
+                Debug.Log("Find Enemy");
+                Enemy.SetState(Enemy.EnemyState.Chase, other.transform);
+            }
         }
     }
 
